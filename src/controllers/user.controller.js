0797851cb8a3deb329d47784aca96a7e6fa6d1f8
Rudra Pratap Padhi <rudrapratappadhi2003@gmail.com>
2024-09-08 00:30:46 +0530

    login and logout controllers are added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,9 +4,25 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        // add refresh token to database which is in user.module.js
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating access and refresh token")
+    }
+}
+
 const registerUser = asyncHandler(async (req,res) => {
     //steps :-
-    // 1 - get details of user from frontend
+    // 1 - get details of user from frontend (req body -> data)
     // 2 - validation - not empty
     // 3 - check if user already exists : username, email
     // 4 - check for images, check for avatar
@@ -84,4 +100,94 @@ const registerUser = asyncHandler(async (req,res) => {
         new ApiResponse(200,createdUser,"User registered successfully")
     )
 })
-export {registerUser}
+
+const loginUser = asyncHandler(async (req,res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // accesss and refresh token
+    // send cookie
+    // return res
+
+    // step - 1 :
+    const {email,username,password} = req.body
+    if(!username || !email) {
+        throw new ApiError(400,"username or email is required")
+    }
+
+    // step - 2 :
+    const user = await User.findOne({
+        $or: [{username},{email}]
+    })
+
+    // step - 3 :
+    if(!user) {
+        throw new ApiError(404, "user doesnot exist")
+    }
+
+    // step - 4:
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid) {
+        throw new ApiError(401,"Invalid User Credentials")
+    }
+
+    // step - 5 :
+    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    // step - 6 :
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    // step - 7 :
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async (req,res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true  // this gives a new response which willl return a true value i.e the refresh token will give a undefined value. If this field is not written there can be a chance of getting the refresh token again.
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200, "User logged out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-        
     } catch (error) {
         console.log("MongoDB conncetion error", error);
         process.exit(1)

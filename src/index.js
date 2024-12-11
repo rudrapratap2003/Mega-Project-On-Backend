139f03f3a7aac7ebd01f84b828e4824cc091b7d4
Rudra Pratap Padhi <rudrapratappadhi2003@gmail.com>
2024-12-11 12:34:10 +0530

    Add resreshToken endpoint

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ connectDB()
     // })
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port : ${process.env.PORT}`);
-        
     })
 })
 .catch((error) => {
